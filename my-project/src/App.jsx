d148d1643af7d11cc68749cdfbea21119a2c79eb
Laksh-Puri0 <usabeeh72@gmail.com>
2025-08-28 02:59:18 +0500

    app jsx updated

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -5,7 +5,7 @@ import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import ServicesPage from "./pages/ServicesPage";
 import BookingPage from "./pages/BookingPage";
-// import ConfirmationPage from "./pages/ConfirmationPage";
+import ConfirmationPage from "./pages/ConfirmationPage";
 
 function App() {
   const [selectedCar, setSelectedCar] = useState(null);
@@ -13,11 +13,17 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage onCarSelect={setSelectedCar} />} />
-      <Route path="/About" element={<AboutPage  />} />
-      <Route path="/Contact" element={<ContactPage  />} />
-      <Route path="/services" element={<ServicesPage selectedCar={selectedCar} />} />
-      <Route path="/booking" element={<BookingPage />} />
-      {/* <Route path="/confirmation" element={<ConfirmationPage />} /> */}
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route
+        path="/services"
+        element={<ServicesPage selectedCar={selectedCar} />}
+      />
+      <Route
+        path="/booking"
+        element={<BookingPage selectedCar={selectedCar} />}
+      />
+      <Route path="/confirmation" element={<ConfirmationPage />} />
     </Routes>
   );
 }
