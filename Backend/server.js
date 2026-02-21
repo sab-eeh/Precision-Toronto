7e90683f7fe138ee4ce3bf0c3fd1fbfd0edc296b
sab-eeh <usabeeh72@gmail.com>
2026-02-21 16:16:22 +0500

    server file updated

--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,7 @@ const PORT = Number(process.env.PORT || 5000);
 (async () => {
   try {
     if (!process.env.MONGO_URI) {
+      console.log("ENV CHECK:", process.env.MONGO_URI);
       throw new Error("MONGO_URI is missing in environment variables");
     }
 
